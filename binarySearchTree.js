8bccfc02ef354b5096a44326d5946cedf6546728
jason murphy <jmurphy.1196@gmail.com>
2020-09-29 00:39:50 -0700

    added BFS and DFS

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -36,17 +36,105 @@ class BinarySearchTree {
           } else {
             currentNode = currentNode.left;
           }
+        } else {
+          return null;
         }
       }
     }
     return this;
+  }
+  find(val) {
+    if (this.root === null) {
+      return null;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (currentNode === null) {
+        return null;
+      }
+      if (val < currentNode.val) {
+        currentNode = currentNode.left;
+      } else if (val > currentNode.val) {
+        currentNode = currentNode.right;
+      } else if (val === currentNode.val) {
+        return currentNode;
+      }
+    }
+  }
+  BFS() {
+    const data = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    return data;
+  }
+  DFS() {
+    const vistedNodes = [];
+    let current = this.root;
+
+    function helper(node) {
+      vistedNodes.push(node.val);
+      if (node.left) {
+        helper(node.left);
+      }
+      if (node.right) {
+        helper(node.right);
+      }
+    }
+    helper(current);
+
+    return vistedNodes;
+  }
+  DFSPre() {
+    const visitedNodes = [];
+    let current = this.root;
+
+    function helper(node) {
+      if (node.left) {
+        helper(node.left);
+      }
+      if (node.right) {
+        helper(node.right);
+      }
+      visitedNodes.push(node.val);
+    }
+    helper(current);
+    return visitedNodes;
+  }
+  DFSInOrder() {
+    const visitedNodes = [];
+    let current = this.root;
+
+    function helper(node) {
+      if (node.left) {
+        helper(node.left);
+      }
+      visitedNodes.push(node.val);
+      if (node.right) {
+        helper(node.right);
+      }
+    }
+    helper(current);
+    return visitedNodes;
   }
 }
 
 let tree = new BinarySearchTree();
 
 tree.insert(5);
+tree.insert(4);
 tree.insert(12);
+tree.insert(13);
 tree.insert(6);
-
-console.log(tree);
+console.log(tree.BFS());
